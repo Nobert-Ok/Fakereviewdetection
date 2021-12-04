214efe2aa247147511e3fe8c71375600290174cf
Chukwuebuka Valentine Okoye-Nobert <o.chukwuebu@alustudent.com>
2021-12-04 22:00:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,19 +6,7 @@ import numpy as np
 import codecs
 import pickle
 import pandas as pd
-from nltk.tokenize import word_tokenize
 import re
-from sklearn.multioutput import MultiOutputClassifier
-from sklearn.model_selection import GridSearchCV
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.pipeline import Pipeline, FeatureUnion
-import nltk
-from sklearn.metrics import classification_report
-from sklearn.base import BaseEstimator,TransformerMixin
-from sklearn.model_selection import train_test_split
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import stopwords
 from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import Tokenizer
 from keras.models import load_model 
